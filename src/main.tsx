43b3cd623f6a977bedeaf32030a56bef1893e9f9
mhbaando <maxamuudyare21@gmail.com>
2026-01-09 15:09:00 +0300

    feat: added browser for able to chaneg pages on teh navigations

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
+import { applyTheme, getSavedThemeMode } from "@/features/themes";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// load all settign then apply tehn start or app
+async function bootStrap() {
+  const mode = getSavedThemeMode();
+  await applyTheme(mode);
+
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+}
+
+bootStrap();
