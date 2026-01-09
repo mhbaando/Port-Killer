0ea7c4e0e3d298618e1a1318662bd47281344ef9
mhbaando <maxamuudyare21@gmail.com>
2026-01-09 15:07:45 +0300

    feat: implemented the app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./main.css";
 import TopHeader from "@/features/header/customHeader";
-
+import { Routes, Route } from "react-router-dom";
+import Settings from "./pages/settings";
+import Dashboard from "./pages/dashboard";
 function App() {
   return (
-    <div className="w-screen h-screen">
+    <div className="w-full h-full flex flex-col bg-background">
       <TopHeader />
+      <div className="flex-1 overflow-hidden">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
     </div>
   );
 }
